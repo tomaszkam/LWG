aebbbfbc334470193befbcd7df9bbee2aece8669
Jonathan Wakely <cxx@kayari.org>
2026-02-23 23:51:11 +0000

    Add [[nodiscard]] to html_utils.h

--- src/html_utils.h
+++ src/html_utils.h
@@ -6,7 +6,7 @@ namespace lwg
 {
 // Replace reserved characters with entities, for use as an attribute value.
 // Use for attributes like title="..." and content="...".
-inline std::string replace_reserved_char(std::string text, char c, std::string_view repl) {
+[[nodiscard]] inline std::string replace_reserved_char(std::string text, char c, std::string_view repl) {
    for (auto p = text.find(c); p != text.npos; p = text.find(c, p+repl.size()))
       text.replace(p, 1, repl);
    return text;
@@ -14,7 +14,7 @@ inline std::string replace_reserved_char(std::string text, char c, std::string_v
 
 // Remove XML elements from the argument to just get the text nodes.
 // Used to generate plain text versions of elements like <title>.
-inline std::string strip_xml_elements(std::string xml) {
+[[nodiscard]] inline std::string strip_xml_elements(std::string xml) {
    for (auto p = xml.find('<'); p != xml.npos; p = xml.find('<', p))
       xml.erase(p, xml.find('>', p) + 1 - p);
    return xml;
@@ -32,15 +32,19 @@ struct xml_element
 };
 
 // Find "<elem>...</elem>" in xml and return as two string views.
+[[nodiscard]]
 std::optional<xml_element> get_element(std::string_view elem, std::string_view xml);
 
 // As above, but only return the inner part.
+[[nodiscard]]
 std::optional<std::string_view> get_element_content(std::string_view elem, std::string_view xml);
 
 // Find attr="..." in xml and return the attribute's value
+[[nodiscard]]
 std::optional<std::string_view> get_attribute(std::string_view attr, std::string_view xml);
 
 // Find <elem attr="..."> in xml and return the attribute's value
+[[nodiscard]]
 std::optional<std::string_view> get_attribute_of(std::string_view attr, std::string_view elem,
                                                  std::string_view xml);
 
